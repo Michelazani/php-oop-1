9ee999afb52c02d97602ca4c3dd08d10de81f2d9
Michelazani <michi.zani220@gmail.com>
2024-01-25 15:49:57 +0100

    first commit

---
+++ models/Movie.php
@@ -0,0 +1,16 @@
+<?php
+    
+    class Movie {
+        public $italianTitle;
+        public $originalTitle;
+        public $genre;
+        public $duration;
+    }
+   
+    //? creo istanza della classe movie
+    $newMovie = new Movie();
+   
+    // visualizzo info
+    var_dump($newMovie);
+
+?>
